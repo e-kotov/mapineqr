9cb7c470f02119ed56ab5fb1fed8956a270f8049
Egor Kotov <kotov.egor@gmail.com>
2024-12-16 11:46:18 +0100

    update docs for mi_data

--- R/data.R
+++ R/data.R
@@ -6,10 +6,8 @@
 #' @param y_source (Optional) A `character` string specifying the source name for the y variable.
 #' @param year A `character` or `integer` specifying the year.
 #' @param level A `character` string specifying the NUTS level ("0", "1", "2", or "3").
-#' @param x_filters A `named list` where the names are the filter fields for the x variable
-#'   and the values are the selected values for those fields. Default is an empty list.
-#' @param y_filters (Optional) A `named list` where the names are the filter fields for the y variable
-#'   and the values are the selected values for those fields. Default is `NULL`.
+#' @param x_filters A `named list` where the names are the filter fields for the x variable and the values are the selected values for those fields. Default is an empty list. To find out which filters to use, use \code{\link{mi_source_filters}} with the desired `source_name`.
+#' @param y_filters (Optional) A `named list` where the names are the filter fields for the y variable and the values are the selected values for those fields. Default is `NULL`. To find out which filters to use, use \code{\link{mi_source_filters}} with the desired `source_name`.
 #' @param limit An `integer` specifying the maximum number of results to return. Default is 2000.
 #'
 #' @return A `tibble` with the following columns:
